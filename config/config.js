76d3e300374720d8e6ba8a918077b6c52a0177e4
Lingam Narendra Reddy <lingam.reddy@gmail.com>
2014-01-30 12:13:41 +0530

    Update config.js
    removed the hardcoded details

--- config/config.js
+++ config/config.js
@@ -1,5 +1,4 @@
 
-
 var path = require('path')
   , rootPath = path.normalize(__dirname + '/..')
   , templatePath = path.normalize(__dirname + '/../app/mailer/templates')
@@ -23,29 +22,30 @@ module.exports = {
       name: 'Nodejs Express Mongoose Demo'
     },
     facebook: {
-      clientID: "1400762646843468",
-      clientSecret: "5b3c56da809c1b2aa9d17a10842737b7",
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
       callbackURL: "http://lnr.thematisse.org:8080/auth/facebook/callback"
     },
     twitter: {
-      clientID: "Gyb8HH4g1FD7UiJdxLAQ",
-      clientSecret: "DeOLRXNk4s8pjmGqzKxH0ViIap4XybPv8Ussgbkj8Gg",
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
       callbackURL: "http://lnr.thematisse.org:8080/auth/twitter/callback"
     },
     github: {
-      clientID: '0172bd042621942a8c83',
-      clientSecret: 'd9a7c2bbe911f8aad832adc0f971331340399763',
+      clientID: 'APP_ID',
+      clientSecret: 'APP_SECRET',
       callbackURL: 'http://lnr.thematisse.org:8080/auth/github/callback'
     },
     google: {
-      clientID: "745646532499.apps.googleusercontent.com",
-      clientSecret: "irewoH5mvthf5d2pA-Kt8JqY",
+      clientID: "APP_ID",
+      clientSecret: "APP_SECRET",
       callbackURL: "http://lnr.thematisse.org:8080/auth/google/callback"
     },
     linkedin: {
-      clientID: "751dc8fzqjgik6",
-      clientSecret: "LGN3sF8WZMGFCN2O",
+      clientID: "CONSUMER_KEY",
+      clientSecret: "CONSUMER_SECRET",
       callbackURL: "http://lnr.thematisse.org:8080/auth/linkedin/callback"
+    }
     }
   },
   test: {
